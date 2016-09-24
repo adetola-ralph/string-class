014e0c4b9b76fc820b42371628773d0c514ff99c
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-09-24 23:18:19 +0100

    add jsdoc comments to hasVowels function

--- src/String.js
+++ src/String.js
@@ -1,4 +1,13 @@
+/**
+ * Has Vowels
+ * 
+ * hasVowel tests if the String calling the function has a vowels
+ * 
+ * @param {void}
+ * @return {Boolean} returns true or false indicating if the string
+ * has a vowel or not
+ */
 String.prototype.hasVowels = function() {
     var inputString = this;
     return /[aeiou]/gi.test(inputString);
-}
+};
