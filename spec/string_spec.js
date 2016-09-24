32fd9888db5378a4b2643655b63683aea57e3df1
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-09-24 22:16:54 +0100

    add spec to test return type of hasVowel function

--- spec/string_spec.js
+++ spec/string_spec.js
@@ -1,3 +1,9 @@
-Describe('hasVowel()', function() {
-    
-})
+describe('hasVowel()', function() {
+    it('shoule always return a boolean', function() {
+        expect(radn()).toEqual(jasmine.any(Boolean));
+    });
+});
+
+function radn (){
+    return true;
+}
