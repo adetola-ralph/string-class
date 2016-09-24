e5dbf6d303520d7373fe224104d825783cef6652
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-09-24 22:55:48 +0100

    add more expect blocks to test hasVowel function

--- spec/string_spec.js
+++ spec/string_spec.js
@@ -5,9 +5,11 @@ describe('hasVowels()', function() {
 
     it('should return true if string has vowels', function() {
         expect('oreofe'.hasVowels()).toBe(true);
+        expect(('AnDeLa').hasVowels()).toBe(true);
     });
 
     it('should return false if string has no vowels', function() {
         expect('why'.hasVowels()).toBe(false);
+        expect(('twyndyllyngs').hasVowels()).toBe(false);
     });
 });
